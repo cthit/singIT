8dba8d48946b467201aefb3f821c03d391bc8353
Ida Dahl <ida.josephine.dahl@gmail.com>
2024-01-30 21:21:08 +0100

    does same as old scraper + song admin features

--- ultrascraper/src/main.rs
+++ ultrascraper/src/main.rs
@@ -1,12 +1,35 @@
-use std::{path::PathBuf, sync::Arc, os::unix::prelude::OsStrExt};
+use std::{fs::create_dir, os::unix::prelude::OsStrExt, path::PathBuf, sync::Arc, vec};
 
-use clap::Parser;
-use eyre::eyre;
-use tokio::{task, fs::{self, File}, io::{BufReader, AsyncBufReadExt}, sync::mpsc};
-
+use clap::{Parser, Subcommand};
+use eyre::{eyre, Context};
+use serde::{Deserialize, Serialize};
+use serde_json;
+use tokio::{
+    fs::{self, File},
+    io::{AsyncBufReadExt, BufReader},
+    sync::mpsc,
+    task,
+};
 #[derive(Parser)]
 struct Opt {
     songs_dir: PathBuf,
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[derive(Subcommand)]
+enum Action {
+    Save {
+        output: PathBuf,
+    },
+    Post {
+        server: String,
+        #[clap(short, long)]
+        token: String,
+    },
+    Admin {
+        output: PathBuf,
+    },
 }
 
 #[tokio::main(flavor = "current_thread")]
@@ -17,8 +40,82 @@ async fn main() -> eyre::Result<()> {
 
     explore_dir(opt.songs_dir, Arc::new(tx));
 
+    let mut songs = vec![];
     while let Some(song) = rx.recv().await {
-        println!("title={:?}, hash={:?}", song.title.unwrap(), song.song_hash);
+        //let song = serde_json::to_string(&song).expect("song should always serialize");
+        // println!("title={:?}, hash={:?}", song.title.unwrap(), song.song_hash);
+        songs.push(song);
+    }
+    let jsongs =
+        serde_json::to_string_pretty(&songs).with_context(|| "failed to serialize song list")?;
+    //println!("{jsongs}");
+
+    match opt.action {
+        Action::Save { output } => {
+            if !output.is_dir() {
+                create_dir(&output).with_context(|| "failed to create output directory")?;
+            }
+            fs::write(output.join("songs.json"), jsongs)
+                .await
+                .with_context(|| "Failed to write to file")?;
+
+            for song in &songs {
+                if let Some(cover) = &song.cover {
+                    let mut cover_from = song.path.clone();
+                    cover_from.pop();
+                    cover_from = cover_from.join(cover);
+                    let cover_to = output.join(&song.song_hash);
+                    if cover_from.is_file() {
+                        std::fs::copy(&cover_from, &cover_to).with_context(|| {
+                            format!("failed to copy song cover from {cover_from:?} to {cover_to:?}")
+                        })?;
+                    } else {
+                        println!("invalid cover {cover_from:?}, song: {:?}", song.title)
+                    }
+                }
+            }
+        }
+        Action::Post { .. } => {
+            todo!("posting to server")
+        }
+        Action::Admin { output } => {
+            fs::write(output, jsongs)
+                .await
+                .with_context(|| "failed to write to file")?;
+            let mut no_video = vec![];
+            let mut no_cover = vec![];
+            let mut no_genre = vec![];
+            for song in songs {
+                if song.cover.is_none() {
+                    let s = (song.path.clone(), song.title.clone(), song.artist.clone());
+                    no_cover.push(s)
+                }
+                if song.video.is_none() && song.bg.is_none() {
+                    let s = (song.path.clone(), song.title.clone(), song.artist.clone());
+                    no_video.push(s)
+                }
+                if song.genre.is_none() {
+                    let s = (song.path.clone(), song.title.clone(), song.artist.clone());
+                    no_genre.push(s)
+                }
+                
+            }
+            let jno_video = serde_json::to_string_pretty(&no_video)
+                .with_context(|| "failed to serialize song list")?;
+            let jno_cover = serde_json::to_string_pretty(&no_cover)
+                .with_context(|| "failed to serialize song list")?;
+            let jno_genre = serde_json::to_string_pretty(&no_genre)
+                .with_context(|| "failed to serialize song list")?;
+            fs::write("no_video.json", jno_video)
+                .await
+                .with_context(|| "failed to write no_video to file")?;
+            fs::write("no_cover.json", jno_cover)
+                .await
+                .with_context(|| "failed to write no_cover to file")?;
+            fs::write("no_genre.json", jno_genre)
+                .await
+                .with_context(|| "failed to write no_cover to file")?;
+        }
     }
 
     Ok(())
@@ -33,13 +130,13 @@ fn explore_dir(path: PathBuf, tx: Arc<mpsc::Sender<Song>>) {
                 explore_dir(entry.path(), Arc::clone(&tx));
             } else if file_type.is_file() {
                 let file_name = entry.file_name();
-                let file_name = file_name.to_str()
+                let file_name = file_name
+                    .to_str()
                     .ok_or_else(|| eyre!("file name not valid utf-8: {:?}", file_name))?;
 
                 if !file_name.ends_with(".txt") {
                     continue;
                 }
-
                 parse_file(entry.path(), Arc::clone(&tx)).await?;
             }
         }
@@ -50,20 +147,21 @@ fn explore_dir(path: PathBuf, tx: Arc<mpsc::Sender<Song>>) {
     task::spawn(inner(path, tx));
 }
 
-
-#[derive(Debug, Default)]
+#[derive(Debug, Default, Serialize, Clone)]
 struct Song {
+    path: PathBuf,
     song_hash: String,
     title: Option<String>,
     artist: Option<String>,
     language: Option<String>,
     genre: Option<String>,
     year: Option<String>,
-    mp3: Option<String>,
+    mp3: Option<String>, // deprecated, change to audio in 2025
     cover: Option<String>,
     video: Option<String>,
     bpm: Option<String>,
     gap: Option<String>,
+    bg: Option<String>,
 }
 
 async fn parse_file(path: PathBuf, tx: Arc<mpsc::Sender<Song>>) -> eyre::Result<()> {
@@ -71,12 +169,13 @@ async fn parse_file(path: PathBuf, tx: Arc<mpsc::Sender<Song>>) -> eyre::Result<
     let mut lines = file.lines();
 
     let file_name = path.file_name().expect("file has a filename");
-    println!("hashing {file_name:?}");
+    //println!("hashing {file_name:?}");
 
     let song_hash = md5::compute(file_name.as_bytes());
     let song_hash = format!("{song_hash:?}");
 
     let mut song = Song {
+        path,
         song_hash,
         ..Default::default()
     };
@@ -95,16 +194,17 @@ async fn parse_file(path: PathBuf, tx: Arc<mpsc::Sender<Song>>) -> eyre::Result<
         };
 
         let field = match key {
-            "TITLE"    => &mut song.title,
-            "ARTIST"   => &mut song.artist ,
-            "LANGUAGE" => &mut song.language ,
-            "GENRE"    => &mut song.genre ,
-            "YEAR"     => &mut song.year ,
-            "MP3"      => &mut song.mp3 ,
-            "COVER"    => &mut song.cover ,
-            "VIDEO"    => &mut song.video ,
-            "BPM"      => &mut song.bpm ,
-            "GAP"      => &mut song.gap,
+            "TITLE" => &mut song.title,
+            "ARTIST" => &mut song.artist,
+            "LANGUAGE" => &mut song.language,
+            "GENRE" => &mut song.genre,
+            "YEAR" => &mut song.year,
+            "MP3" => &mut song.mp3,
+            "COVER" => &mut song.cover,
+            "VIDEO" => &mut song.video,
+            "BPM" => &mut song.bpm,
+            "GAP" => &mut song.gap,
+            "BACKGROUND" => &mut song.bg,
             _ => continue,
         };
 
---
+++ ultrascraper/todo.md
@@ -0,0 +1,16 @@
+# TODO
+
+ - [x] recursively search song ridectory (done???)
+ - [x] read all song fields
+ - [ ] read genre as a comma separated list
+ - [ ] try to hash same as old scraper
+ - [ ] post to server, 
+ - [x] or output to file
+ - [x] images (save 'em 
+ - [ ] and post 'em)
+ 
+## For song mantainer  (nice to haves)
+ - [x] list songs w/out bg or video
+ - [x] list songs w/out covers
+ - [x] list songs w/out genre
+ - [ ] list songs w/ similar names (as song artist pairs)
